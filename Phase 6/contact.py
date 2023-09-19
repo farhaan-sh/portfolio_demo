3538a1f764eecf3a961fe0a42b30db01dcc04bbd
farhaan-sh <shf.cse@gmail.com>
2023-09-19 19:35:44 +0600

    Phase 6 Updated and Fixed v1.2

--- Phase 6/contact.py
+++ Phase 6/contact.py
@@ -4,9 +4,37 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 from streamlit_option_menu import option_menu
 
+
 # st.set_page_config(page_title="My WebPage", page_icon=":tada:", layout="wide")
 
 
 def app():
     st.title(f"You have selected Contact Page")
 
+    def local_css(file_name):
+        with open(file_name) as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+    local_css("style/style.css")
+
+    # ---- CONTACT ----
+    with st.container():
+        st.write("---")
+        st.header("Get In Touch With Me!")
+        st.write("##")
+
+        # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
+        contact_form = """
+         <form action="https://formsubmit.co/shf.cse@gmail.com" method="POST">
+             <input type="hidden" name="_captcha" value="false">
+             <input type="text" name="name" placeholder="Your name" required>
+             <input type="email" name="email" placeholder="Your email" required>
+             <textarea name="message" placeholder="Your message here" required></textarea>
+             <button type="submit">Send</button>
+         </form>
+         """
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.markdown(contact_form, unsafe_allow_html=True)
+        with right_column:
+            st.empty()
--- Phase 6/main.py
+++ Phase 6/main.py
@@ -34,12 +34,12 @@ class MultiApp:
     def __init__(self):
         self.apps = []
 
-    def add_app(self, title, func):
-
-        self.apps.append({
-            "title": title,
-            "function": func
-        })
+    # def add_app(self, title, func):
+    #
+    #     self.apps.append({
+    #         "title": title,
+    #         "function": func
+    #     })
 
     def run():
         # app = st.sidebar(
