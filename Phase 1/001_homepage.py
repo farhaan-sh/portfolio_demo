b4438812e6bfd68a8981317571a48134e9d00fd8
farhaan-sh <shf.cse@gmail.com>
2023-09-19 17:40:16 +0600

    Phase 1 Initiated

---
+++ Phase 1/001_homepage.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(page_title="My WebPage", page_icon=":tada:", layout="wide")
+
+
+
+st.subheader("Hi, I am Farhaan :wave:")
+st.title("A Newbie ML Engineer from Bangladesh")
+st.write("I am An enthusiastic AI and ML "
+         "researcher with a strong open-source "
+         "contribution drive. Effective communicator "
+         "& a natural writer. Seeking opportunities in "
+         "the field of AI for innovation and collaboration")
+st.write("[Get Connected via LinkedIn >](https://www.linkedin.com/in/shfarhaan/)")
+st.markdown(
+    """
+    # This is an H1 Header
+    ## This is an H2 Header
+    
+    """
+)
